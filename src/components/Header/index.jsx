8016d60a178f487999f050cefc7693e1e9e44fec
Ana Paula <prabelo79@gmail.com>
2024-07-18 17:56:08 -0300

    first commit: inicializa projeto

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import styles from './header.module.css'
+
+export function Header(){
+    return (
+        <header className={styles.cabecalho}>
+            <p>header</p>
+        </header>
+    )
+}
